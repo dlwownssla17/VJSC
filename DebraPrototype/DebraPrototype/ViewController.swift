8a55966f2c9f396bc3347c854da2a01719da93eb
chadnach1 <chadn@seas.upenn.edu>
2016-11-23 17:12:16 -0500

    First Commit

---
+++ DebraPrototype/DebraPrototype/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  DebraPrototype
+//
+//  Created by Chad Nachiappan on 11/23/16.
+//  Copyright © 2016 Chad Nachiappan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+//        // Colored Gradient
+//        CAGradientLayer *gradient = [CAGradientLayer layer];
+//        gradient.frame = self.view.bounds;
+//        gradient.colors = [NSArray arrayWithObjects: (id)[[UIColor colorWithRed:0.776 green:0.263 blue:0.988 alpha:1] /*#c643fc*/ CGColor], (id)[[UIColor colorWithRed:0.937 green:0.302 blue:0.714 alpha:1] /*#ef4db6*/ CGColor], nil];
+//        [self.view.layer insertSublayer:gradient atIndex:0];
+//        
+//        UILabel *headingLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 240, 300, 30)];
+//        headingLabel.text = @"Not to fear, Debra is here!";
+//        headingLabel.textColor = [UIColor whiteColor];
+//        headingLabel.textAlignment = NSTextAlignmentCenter;
+//        headingLabel.tag = 10;
+//        headingLabel.font = [UIFont fontWithName:@"AppleSDGothicNeo-Regular" size:20.0];
+//        headingLabel.hidden = NO;
+//        [self.view addSubview:headingLabel];
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
