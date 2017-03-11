f39fc322d78ce512ab60d121c765c855c885ef2c
chadnach1 <chadn@seas.upenn.edu>
2017-03-11 04:21:16 -0500

    under construction scene added to community tab

--- DebraTabs/DebraTabs/DataViewController.swift
+++ DebraTabs/DebraTabs/DataViewController.swift
@@ -11,8 +11,29 @@ import UIKit
 
 class DataViewController: UIViewController {
     
+    public var blueColor: UIColor = UIColor(red: CGFloat(0/255.0), green: CGFloat(122/255.0), blue: CGFloat(255/255.0), alpha: CGFloat(1.0))
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let displayWidth: CGFloat = self.view.frame.width
+        let displayHeight: CGFloat = self.view.frame.height
+        
+        let codedLabel:UILabel = UILabel()
+        codedLabel.frame = CGRect(x: 100, y: 100, width: displayWidth, height: 200)
+        codedLabel.textAlignment = .center
+        codedLabel.text = "Under Construction! Check back later please."
+        codedLabel.numberOfLines = 5
+        codedLabel.textColor = blueColor
+        codedLabel.font=UIFont.systemFont(ofSize: 40)
+        codedLabel.backgroundColor=UIColor.clear
+        
+        self.view.addSubview(codedLabel)
+        codedLabel.translatesAutoresizingMaskIntoConstraints = false
+        codedLabel.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        codedLabel.widthAnchor.constraint(equalToConstant: displayWidth).isActive = true
+        codedLabel.centerXAnchor.constraint(equalTo: codedLabel.superview!.centerXAnchor).isActive = true
+        codedLabel.centerYAnchor.constraint(equalTo: codedLabel.superview!.centerYAnchor).isActive = true
     }
     
     override func didReceiveMemoryWarning() {
