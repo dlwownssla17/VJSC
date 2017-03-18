df16971b831f063d04466f7f42b869dbde8c26ed
Spiro Metaxas <spirometaxas@levyvlan502.3964.wlan.wireless-resnet.upenn.edu>
2017-03-18 11:53:43 -0400

    changes

--- DebraTabs/DebraTabs/AppSettings.swift
+++ DebraTabs/DebraTabs/AppSettings.swift
@@ -11,9 +11,9 @@ import Foundation
 class Settings {
 
     //static let serverURL:String = "http://130.91.135.160:8000/"
-    static let serverURL:String = "http://165.123.207.125:8000/"
+    //static let serverURL:String = "http://165.123.207.125:8000/"
     
-//    static let serverURL:String = "http://165.123.203.89:8000/"
+    static let serverURL:String = "http://165.123.203.89:8000/"
     
     
     //static let serverURL:String = "http://130.91.134.209:8000/"
